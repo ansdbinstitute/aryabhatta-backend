fe5f1dbcea76664f4b84b5ab2ffd5d4234092c6e
ANSDB Developer <developer@ansdb.org>
2026-04-10 19:35:13 +0530

    fix: allow Cache-Control, Pragma, and Expires headers in CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -23,7 +23,7 @@ const config: Core.Config.Middlewares = [
         'https://app.ansdb.org',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Cache-Control', 'Pragma', 'Expires'],
       keepHeaderOnError: true,
     },
   },
